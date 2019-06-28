a8deeb752974fa92f2c5d0f6b7cb677f134fb47c
Kristoffer Risa <kristofferrisa@gmail.com>
2019-06-28 20:38:48 +0200

    added navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav>
+        <nuxt-link to="/">Hjem</nuxt-link>
+        <nuxt-link to="/About">Om</nuxt-link>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style>
+nav {
+  overflow: hidden;
+  position: fixed; /* Set the navbar to fixed position */
+  top: 0; /* Position the navbar at the top of the page */
+  width: 100%; /* Full width */
+  text-align: center;
+  /* text-decoration: underline; */
+  margin-top:25px; 
+}
+
+nav > a {
+    margin: 25px;
+    color: black;
+    font-size: 1.5rem;
+}
+
+</style>
